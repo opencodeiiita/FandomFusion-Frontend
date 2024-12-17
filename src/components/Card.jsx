51b16ff0b58d1abb17cb0a195a4e598727012b6f
CalC <maheshwarivinayak90@gmail.com>
2024-12-17 20:05:00 +0530

    created card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,28 @@
+import { FaPlaystation } from "react-icons/fa";
+import { FaXbox } from "react-icons/fa";
+import { FaComputer } from "react-icons/fa6";
 
 
-
-
-export const Card = ({ title, duration, genre, rating, releaseDate, imgUrl }) => {
+export const Card = ({type, title, duration, genre, rating, releaseDate, imgUrl, episodes, platform }) => {
+    const icons = {
+        "pc": <FaComputer />,
+        "xbox": <FaXbox />,
+        "playstation": <FaPlaystation />
+    }
     return (
         <div className="bg-cardBG flex flex-col w-[300px] h-[520px] hover:scale-105 rounded-xl">
             <div className="h-3/4 rounded-t-xl w-full">
-                <img src={imgUrl} alt="Movie Cover Image" className="w-full h-full rounded-t-xl" />
+                <img src={imgUrl} alt="Cover Image" className="w-full h-full rounded-t-xl" />
             </div>
             <div className="h-1/4 flex flex-col p-4">
                 <p className="text-2xl text-white font-sans">{title}</p>
-                <div className="w-full flex justify-between">
-                <p className="text-cardSubtitleColor text-sm">{duration} / {genre} / {releaseDate}</p>
-                <p className=" text-[1rem] bg-yellow-400 px-2 rounded text-black">{rating}</p>
+                <p className="text-cardSubtitleColor text-sm">{type}</p>
+                <div className="w-full flex items-center justify-between">
+                <p className="text-cardSubtitleColor text-sm">{type==="Movie" && duration+"/"}{type==="Anime" && episodes+" episodes/"}  {genre}/ {releaseDate}</p>
+                <p className={`flex gap-x-1 ${type==="Movie" && "bg-yellow-400 text-[1rem] text-black"} ${type==="Anime" && "bg-yellow-400 text-[1rem] text-black"} ${type==="Game" && "text-xl text-cardSubtitleColor"}   px-2 rounded `}>{type==="Movie" && rating}{type==="Anime" && rating}{type==="Game" && platform.map((platform) => icons[platform])}</p>
                 </div>
             </div>
-            <button className="w-full self-center bg-cardButtonColor hover:bg-cardButtonColor/60 text-white py-2 rounded-b-xl">Watch Now</button>
+            <button className="w-full self-center bg-cardButtonColor hover:bg-cardButtonColor/60 text-white py-2 rounded-b-xl">Add to Profile</button>
 
         </div>
     )
