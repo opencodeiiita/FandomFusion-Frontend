a8a0c5d54812c87e3c1ecf77d5d0a1dd4ff429f3
Gamin8ing <anim8tor.ing@gmail.com>
2024-12-18 19:17:50 +0530

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
-import './App.css';
-import Home from './pages/Home.jsx'; 
-import NotFound from './pages/404.jsx';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import NotFound from "./pages/404.jsx";
+import Contributors from "./pages/Contributors.jsx";
 const App = () => {
-return (
-	<Router>
-		<div className="App">
-		  {/* Main routes defined here */}
-		  <Routes>
-			<Route path="/" element={<Home />} />
-		  </Routes>
-		  <Route path="*" element={NotFound}/>
-		</div>
-	</Router>
-);
-};  
+	return (
+		<Router>
+			{/* Main routes defined here */}
+			<Routes>
+				<Route path="/" element={<h1>Hello world</h1>} />{" "}
+				{/* TODO: Change this with actual Home page */}
+				<Route path="/contributors" element={<Contributors />} />
+				{/* 404 page */}
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</Router>
+	);
+};
 export default App;
-
-
-
